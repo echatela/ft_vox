f14c86210a343cd0728a6262513e34636c6ee58d
D3chain <edchatelain9@gmail.com>
2026-08-31 16:48:10 +0200

    feat: data set in shader

--- src/world/chunk.cpp
+++ src/world/chunk.cpp
@@ -1,6 +1,4 @@
 #include "chunk.hpp"
-#include "glm/ext/matrix_float4x4.hpp"
-#include "glm/ext/matrix_transform.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "render/shader.hpp"
 
@@ -12,73 +10,80 @@ Chunk::Chunk()
 	setupMesh();
 }
 
-void Chunk::draw(Shader& shader) const
+// TODO : could put x / y / z out of loop to make less calcul
+void Chunk::draw(Shader& shader)
 {
+	const int yOffset = static_cast<float>(kChunkHeight) / 2;
+
 	glBindVertexArray(_vao);
+	glm::vec3 pos;
 	for (int i = 0; i < kChunkSize; i++)
 	{
-		drawCube(shader, i);
+		pos.x = i % kChunkWidth;
+		pos.y = i / kChunkWidth % kChunkHeight - yOffset;
+		pos.z = i / (kChunkWidth * kChunkHeight) % kChunkWidth;
+
+		checkCube(pos);
+	}
+	shader.use();
+	glDrawArrays(GL_QUADS, 0, 36);
+}
+
+enum Face : uint8_t
+{
+	kFaceRight,
+	kFaceLeft,
+	kFaceUp,
+	kFaceDown,
+	kFaceForward,
+	kFaceBack,
+	kFaceCount
+};
+
+constexpr glm::u8vec3 kCorners[kFaceCount][4] = {
+    {{1, 0, 1}, {1, 0, 0}, {1, 1, 0}, {1, 1, 1}}, // +X
+    {{0, 0, 0}, {0, 0, 1}, {0, 1, 1}, {0, 1, 0}}, // -X
+    {{0, 1, 1}, {1, 1, 1}, {1, 1, 0}, {0, 1, 0}}, // +Y
+    {{0, 0, 0}, {1, 0, 0}, {1, 0, 1}, {0, 0, 1}}, // -Y
+    {{0, 0, 1}, {1, 0, 1}, {1, 1, 1}, {0, 1, 1}}, // +Z
+    {{1, 0, 0}, {0, 0, 0}, {0, 1, 0}, {1, 1, 0}}, // -Z
+};
+
+void Chunk::checkCube(const glm::vec3& pos)
+{
+	for (int face = 0; face < kFaceCount; face++)
+	{
+		checkFace(face, pos);
 	}
 }
 
-static float vertices[] = {
-    /* pos */ 0.0f, 0.0f, 0.0f,  /* vt */ 0.0f, 0.0f, /* pos */ 1.0f, 0.0f, 0.0f, /* vt */ 1.0f, 0.0f,
-    /* pos */ 1.0f, 1.0f, 0.0f,  /* vt */ 1.0f, 1.0f, /* pos */ 1.0f, 1.0f, 0.0f, /* vt */ 1.0f, 1.0f,
-    /* pos */ 0.0f, 1.0f, 0.0f,  /* vt */ 0.0f, 1.0f, /* pos */ 0.0f, 0.0f, 0.0f, /* vt */ 0.0f, 0.0f,
-
-	/* pos */ 0.0f, 0.0f, -1.0f,  /* vt */ 1.0f, 0.0f, /* pos */ 1.0f, 0.0f, -1.0f, /* vt */ 0.0f, 0.0f,
-    /* pos */ 1.0f, 1.0f, -1.0f,  /* vt */ 0.0f, 1.0f, /* pos */ 1.0f, 1.0f, -1.0f, /* vt */ 0.0f, 1.0f,
-    /* pos */ 0.0f, 1.0f, -1.0f,  /* vt */ 1.0f, 1.0f, /* pos */ 0.0f, 0.0f, -1.0f, /* vt */ 1.0f, 0.0f,
-
-	/* pos */ 0.0f, 1.0f, -1.0f,  /* vt */ 0.0f, 1.0f, /* pos */ 0.0f, 1.0f, 0.0f, /* vt */ 1.0f, 1.0f,
-    /* pos */ 0.0f, 0.0f, 0.0f,  /* vt */ 1.0f, 0.0f, /* pos */ 0.0f, 0.0f, 0.0f, /* vt */ 1.0f, 0.0f,
-    /* pos */ 0.0f, 0.0f, -1.0f,  /* vt */ 0.0f, 0.0f, /* pos */ 0.0f, 1.0f, -1.0f, /* vt */ 0.0f, 1.0f,
-
-	/* pos */ 1.0f, 1.0f, -1.0f,  /* vt */ 1.0f, 1.0f, /* pos */ 1.0f, 1.0f, 0.0f, /* vt */ 0.0f, 1.0f,
-    /* pos */ 1.0f, 0.0f, 0.0f,  /* vt */ 0.0f, 0.0f, /* pos */ 1.0f, 0.0f, 0.0f, /* vt */ 0.0f, 0.0f,
-    /* pos */ 1.0f, 0.0f, -1.0f,  /* vt */ 1.0f, 0.0f, /* pos */ 1.0f, 1.0f, -1.0f, /* vt */ 1.0f, 1.0f,
-
-	/* pos */ 0.0f, 0.0f, 0.0f,  /* vt */ 0.0f, 0.0f, /* pos */ 1.0f, 0.0f, 0.0f, /* vt */ 1.0f, 0.0f,
-    /* pos */ 1.0f, 0.0f, -1.0f,  /* vt */ 1.0f, 1.0f, /* pos */ 1.0f, 0.0f, -1.0f, /* vt */ 1.0f, 1.0f,
-    /* pos */ 0.0f, 0.0f, -1.0f,  /* vt */ 0.0f, 1.0f, /* pos */ 0.0f, 0.0f, 0.0f, /* vt */ 0.0f, 0.0f,
-
-	/* pos */ 0.0f, 1.0f, 0.0f,  /* vt */ 0.0f, 0.0f, /* pos */ 1.0f, 1.0f, 0.0f, /* vt */ 1.0f, 0.0f,
-    /* pos */ 1.0f, 1.0f, -1.0f,  /* vt */ 1.0f, 1.0f, /* pos */ 1.0f, 1.0f, -1.0f, /* vt */ 1.0f, 1.0f,
-    /* pos */ 0.0f, 1.0f, -1.0f,  /* vt */ 0.0f, 1.0f, /* pos */ 0.0f, 1.0f, 0.0f, /* vt */ 0.0f, 0.0f};
-
-// TODO : could put x / y / z out of loop to make less calcul
-void Chunk::drawCube(Shader& shader, int i) const
+void Chunk::checkFace(int face, const glm::vec3& pos)
 {
-	glm::vec3 pos;
-	glm::mat4 model(1.0f);
-	const int yOffset = static_cast<float>(kChunkHeight) / 2;
-
-	pos.x = i % kChunkWidth;
-	pos.y = i / kChunkWidth % kChunkHeight - yOffset;
-	pos.z = -(i / (kChunkWidth * kChunkHeight) % kChunkWidth);
-
-	model = glm::translate(model, pos);
-	shader.setMat4("model", model);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	for (int i = 0; i < 4; i++)
+		_vertices.push_back({pos, face, i});
 }
 
 void Chunk::setupMesh()
 {
-	unsigned int VBO;
 	glGenVertexArrays(1, &_vao);
-	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &_vbo);
 
 	glBindVertexArray(_vao);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(_vertices), _vertices.data(),
+	             GL_STATIC_DRAW);
 
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
 	                      (void*)0);
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
-	                      (void*)(3 * sizeof(float)));
+	// face attribute
+	glVertexAttribPointer(1, 1, GL_INT, GL_FALSE, sizeof(Vertex),
+	                      (void*)offsetof(Vertex, face));
 	glEnableVertexAttribArray(1);
+	// iteration
+	glVertexAttribPointer(2, 1, GL_INT, GL_FALSE, sizeof(Vertex),
+	                      (void*)offsetof(Vertex, i));
+	glEnableVertexAttribArray(2);
 }
--- src/world/chunk.hpp
+++ src/world/chunk.hpp
@@ -1,22 +1,35 @@
 #pragma once
 
 #include "render/shader.hpp"
+#include <vector>
 static constexpr int kChunkWidth = 16;
 static constexpr int kChunkHeight = 256;
 static constexpr int kChunkSize = kChunkWidth * kChunkHeight * kChunkWidth;
 
+struct Vertex
+{
+	glm::vec3 pos;
+	int       face;
+	int       i;
+};
+
 class Chunk
 {
-	bool         _voxels[kChunkSize]{true};
+	bool _voxels[kChunkSize]{true};
+
 	unsigned int _vao;
+	unsigned int _vbo;
+
+	std::vector<Vertex> _vertices;
 
 public:
 	Chunk();
 
-	void draw(Shader& shader) const;
+	void draw(Shader& shader);
 	// bool at(int x, int y, int z) const;
 
 private:
-	void drawCube(Shader& shader, int i) const;
+	void checkCube(const glm::vec3& pos);
+	void checkFace(int face, const glm::vec3& pos);
 	void setupMesh();
 };
