81bcd3724f95f3645c5e36333b5103e5b6037b44
echatela <ed_srv@proton.me>
2026-09-18 19:47:11 +0200

    indentation

--- src/world/chunk_manager.cpp
+++ src/world/chunk_manager.cpp
@@ -1,15 +1,13 @@
 #include "chunk_manager.hpp"
-#include "glm/ext/matrix_float4x4.hpp"
 #include "glm/ext/vector_int2_sized.hpp"
 #include "loader/resource_manager.hpp"
 #include "render/texture_2d_array.hpp"
 #include "scene/material.hpp"
-#include <exception>
 
-constexpr int kLoadDistance = 10;
+constexpr int  kLoadDistance = 10;
 constexpr auto kLoadRange = kLoadDistance * 2 + 1;
 
-//constexpr int kViewDistance = 5;
+// constexpr int kViewDistance = 5;
 
 // TODO : Destructor should empty node and free map
 ChunkManager::ChunkManager()
@@ -21,68 +19,14 @@ ChunkManager::ChunkManager()
 
 	updateChunks({0, 0, 0}, true);
 	// loadAround({0, 0, 0});
-
 }
 
-
-void ChunkManager::_swapRange(const glm::ivec2& oldPos, const glm::ivec2& newPos)
-{
-	glm::ivec2 move = newPos - oldPos;
-	glm::ivec2 loadPos; //, unloadPos;
-
-
-	const int		xSign = move.x >= 0 ? 1 : -1;
-	const int		ySign = move.y >= 0 ? 1 : -1;
-
-	unsigned int fullRow = std::min(std::abs(move.y), kLoadRange);
-
-	// Processing each full row
-	for (uint row = 0; row < fullRow; row ++)
-	{
-		for (int xRange = -kLoadDistance; xRange <= kLoadDistance; xRange++)
-		{
-			loadPos = glm::ivec2(	newPos.x + xRange,
-									newPos.y + (kLoadDistance - row) * ySign);
-			_loadChunk(loadPos);
-			_unloadChunk(oldPos - (loadPos - newPos)); // symmetrical unload
-		}
-	}
-	//Processing the leftovers
-	for (uint yLeft = 0; yLeft < kLoadRange - fullRow; yLeft++)
-	{
-		for (uint xLeft = 0; xLeft < (uint)abs(move.x); xLeft ++)
-		{
-			loadPos = glm::ivec2(	newPos.x + (kLoadDistance - xLeft) * xSign,
-									newPos.y - (kLoadDistance - yLeft) * ySign);
-			_loadChunk(loadPos);
-			_unloadChunk(oldPos - (loadPos - newPos));
-		}
-	}
-}
-
-// Load + Unload a chunk, keeping the old allocated Chunk pointer
-// No need to remove from _tree because it uses the Chunk*
-void ChunkManager::_swapChunk(const glm::ivec2& oldPos, const glm::ivec2& newPos)
-{
-	auto it = _chunks.find(oldPos);
-
-	if (it == _chunks.end())
-		return;
-
-	//reuse allocated chunk
-	*(it->second) = Chunk(newPos * 16, _material);
-
-	//insert new pair & remove old
-	_chunks[newPos] = it->second;
-	_chunks.erase(it);
-
-}
-
-void ChunkManager::updateChunks(const glm::vec3 &pos, bool preload)
+void ChunkManager::updateChunks(const glm::vec3& pos, bool preload)
 {
 	static glm::ivec2 lastPos;
 
-	glm::ivec2 newPos = glm::ivec2{std::floor(pos.x / 16), std::floor(pos.z / 16)};
+	glm::ivec2 newPos =
+	    glm::ivec2{std::floor(pos.x / 16), std::floor(pos.z / 16)};
 
 	if (preload)
 	{
@@ -94,9 +38,61 @@ void ChunkManager::updateChunks(const glm::vec3 &pos, bool preload)
 	{
 		_swapRange(lastPos, newPos);
 	}
-	
-	lastPos = newPos;
 
+	lastPos = newPos;
+}
+
+void ChunkManager::_swapRange(const glm::ivec2& oldPos,
+                              const glm::ivec2& newPos)
+{
+	glm::ivec2 move = newPos - oldPos;
+	glm::ivec2 loadPos; //, unloadPos;
+
+	const int xSign = move.x >= 0 ? 1 : -1;
+	const int ySign = move.y >= 0 ? 1 : -1;
+
+	unsigned int fullRow = std::min(std::abs(move.y), kLoadRange);
+
+	// Processing each full row
+	for (uint row = 0; row < fullRow; row++)
+	{
+		for (int xRange = -kLoadDistance; xRange <= kLoadDistance; xRange++)
+		{
+			loadPos = glm::ivec2(newPos.x + xRange,
+			                     newPos.y + (kLoadDistance - row) * ySign);
+			_loadChunk(loadPos);
+			_unloadChunk(oldPos - (loadPos - newPos)); // symmetrical unload
+		}
+	}
+	// Processing the leftovers
+	for (uint yLeft = 0; yLeft < kLoadRange - fullRow; yLeft++)
+	{
+		for (uint xLeft = 0; xLeft < (uint)abs(move.x); xLeft++)
+		{
+			loadPos = glm::ivec2(newPos.x + (kLoadDistance - xLeft) * xSign,
+			                     newPos.y - (kLoadDistance - yLeft) * ySign);
+			_loadChunk(loadPos);
+			_unloadChunk(oldPos - (loadPos - newPos));
+		}
+	}
+}
+
+// Load + Unload a chunk, keeping the old allocated Chunk pointer
+// No need to remove from _tree because it uses the Chunk*
+void ChunkManager::_swapChunk(const glm::ivec2& oldPos,
+                              const glm::ivec2& newPos)
+{
+	auto it = _chunks.find(oldPos);
+
+	if (it == _chunks.end())
+		return;
+
+	// reuse allocated chunk
+	*(it->second) = Chunk(newPos * 16, _material);
+
+	// insert new pair & remove old
+	_chunks[newPos] = it->second;
+	_chunks.erase(it);
 }
 
 void ChunkManager::_loadAround(const glm::ivec2& pos)
@@ -105,36 +101,12 @@ void ChunkManager::_loadAround(const glm::ivec2& pos)
 
 	for (vec.y = pos.y - kLoadDistance; vec.y <= pos.y + kLoadDistance; vec.y++)
 	{
-		for (vec.x = pos.x - kLoadDistance; vec.x <= pos.x + kLoadDistance; vec.x++)
+		for (vec.x = pos.x - kLoadDistance; vec.x <= pos.x + kLoadDistance;
+		     vec.x++)
 		{
 			_loadChunk(vec);
 		}
 	}
-}
-
-void ChunkManager::_draw(RenderContext& context) const
-{
-	(void)context;
-	// the const & is really important for performance !!!
-	// for (const auto& chunkIt : _chunks)
-	// {
-	// 	chunkIt.second._draw(matrix);
-	// }
-}
-
-void ChunkManager::setMaterial(const Material& mat)
-{
-	_material = mat;
-}
-
-const Material& ChunkManager::getMaterial() const
-{
-	return _material;
-}
-
-unsigned int ChunkManager::getSize() const
-{
-	return _chunks.size();
 }
 
 void ChunkManager::_loadChunk(const glm::i32vec2& pos)
@@ -155,7 +127,6 @@ void ChunkManager::_loadChunk(const glm::i32vec2& pos)
 
 void ChunkManager::_unloadChunk(const glm::i32vec2& pos)
 {
-
 	if (_isLoaded(pos))
 	{
 		Chunk* ptr = _chunks[pos];
@@ -170,8 +141,32 @@ void ChunkManager::_unloadChunk(const glm::i32vec2& pos)
 	}
 }
 
+void ChunkManager::_draw(RenderContext& context) const
+{
+	(void)context;
+	// the const & is really important for performance !!!
+	// for (const auto& chunkIt : _chunks)
+	// {
+	// 	chunkIt.second._draw(matrix);
+	// }
+}
 
 bool ChunkManager::_isLoaded(const glm::i32vec2& pos)
 {
 	return (_chunks.find(pos) != _chunks.end());
+}
+
+void ChunkManager::setMaterial(const Material& mat)
+{
+	_material = mat;
+}
+
+const Material& ChunkManager::getMaterial() const
+{
+	return _material;
+}
+
+unsigned int ChunkManager::getSize() const
+{
+	return _chunks.size();
 }
