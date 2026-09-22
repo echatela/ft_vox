f77247edbe59def42f8aeab2acfb30348deeb888
Theo Fiette <tfiette@f6r13s13.clusters.42paris.fr>
2026-09-22 16:22:47 +0200

    removed process toggle for labels in menu

--- src/scene/label.cpp
+++ src/scene/label.cpp
@@ -134,16 +134,16 @@ void Label::_constructMesh()
 		endX += xStep;
 
 		// v0
-		coords.push_back(glm::vec2(startX, startY));
+		coords.push_back(glm::vec2(floor(startX), startY));
 		coords.push_back(glm::vec2(letter.offset / kBitmapWidth, 1));
 		// v1
-		coords.push_back(glm::vec2(endX, startY));
+		coords.push_back(glm::vec2(floor(endX), startY));
 		coords.push_back(glm::vec2((letter.offset + letter.width) / kBitmapWidth, 1));
 		// v2
-		coords.push_back(glm::vec2(endX, endY));
+		coords.push_back(glm::vec2(floor(endX), endY));
 		coords.push_back(glm::vec2((letter.offset + letter.width) / kBitmapWidth, 0));
 		// v3
-		coords.push_back(glm::vec2(startX, endY));
+		coords.push_back(glm::vec2(floor(startX), endY));
 		coords.push_back(glm::vec2(letter.offset / kBitmapWidth, 0));
 
 		for (const int vertIndex : {0, 1, 3, 1, 2, 3})
@@ -200,7 +200,8 @@ Label::Label(std::string text, unsigned int size, glm::vec3 color)
 	_size(size),
 	_color(color)
 {
-	_constructMesh();
+	if (text.length() && size)
+		_constructMesh();
 
 	ResourceManager& rm = ResourceManager::instance();
 	const ATexture* texturePtr = rm.get<ATexture>(ResourceId::TEXTURE_FONT);
